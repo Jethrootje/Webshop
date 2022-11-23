0aa163ad73182828a501955286c613157d5c4d58
Jethro <jethro02@live.nl>
2022-11-23 22:52:27 +0100

    DbContext, Classes and Controllers
    Customer, OrderDetail, Order and Product

---
+++ BusinessLogic/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BusinessLogic
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string NamePreposition { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ DataAccessLayer/Data/WebshopContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DataAccessLayer.Models;
+
+namespace DataAccessLayer.Data
+{
+    public class WebshopContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=WebshopV1;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
